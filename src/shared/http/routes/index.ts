e1e3e2afe109582c7d82ef08c6e599737b1bdd39
ROMUALDO-TXT <lucasrl2003@gmail.com>
2021-12-06 14:54:11 -0300

    Customers added

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,8 @@ import sessionsRouter from '@modules/users/routes/Sessions.routes';
 import usersRouter from '@modules/users/routes/Users.routes';
 import passwordRouter from '@modules/users/routes/Passwords.routes';
 import { Router } from 'express';
+import profileRouter from '@modules/users/routes/Profile.routes';
+import customersRouter from '@modules/costumers/routes/Customers.routes';
 
 const routes = Router();
 
@@ -13,6 +15,10 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 
 routes.use('/passwords', passwordRouter);
+
+routes.use('/profile', profileRouter);
+
+routes.use('/customers',customersRouter)
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'hello world' });
